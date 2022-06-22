1703029b9436a6b71b30db210c2ddb4e415f16d9
Matvey Dorofeev <matiaso@bk.ru>
2022-06-22 12:17:04 +0300

    ch-2

--- data_from_sheets/take_data_and_insert.py
+++ data_from_sheets/take_data_and_insert.py
@@ -1,18 +1,16 @@
 from datetime import date as dt
-import time
 import psycopg2
 import requests
+import time
 
-from typing import Any
-
-from read_Google_sheets import read
+from read_google_sheets import read
 from bot_telegram.bot import bot
 
-data_central_bank: Any = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+data_central_bank = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 VALUE_RUB: float = data_central_bank['Valute']['USD']['Value']
-data_google_sheet: list = read()
-data: list = data_google_sheet[1:]
-all_id: list = []
+data_google_sheet = read()
+data = data_google_sheet[1:]
+all_id = []
 
 
 def insert(*args: tuple) -> None:
@@ -20,10 +18,10 @@ def insert(*args: tuple) -> None:
     Выполнение SQL-запроса для вставки данных в таблицу
     """
     pk, order, price_d, price_r, date = args
+    item_tuple = (pk, order, price_d, price_r, date)
     try:
         insert_query = """ INSERT INTO page_orders (id, zakaz, price_d, price_r, date)
         VALUES (%s, %s, %s, %s, %s)"""
-        item_tuple = (pk, order, price_d, price_r, date)
         cursor.execute(insert_query, item_tuple)
         connection.commit()
     except (Exception, psycopg2.Error) as error:
@@ -71,18 +69,18 @@ while True:
 
         cursor = connection.cursor()
         print("Запись пошла")
-        
+
         for datum in data:
             pk, order, price_d, date = datum
             all_id.append(int(pk))
             price_d = float(price_d)
             price_r = round(price_d*VALUE_RUB, 2)
             cursor.execute(f"SELECT * from page_orders where id = {pk}")
-            new_datum: tuple[str, str, float, float, str] = (pk,
-                                                             order,
-                                                             price_d,
-                                                             price_r,
-                                                             date)
+            new_datum = (pk,
+                         order,
+                         price_d,
+                         price_r,
+                         date)
             if cursor.fetchall() == []:
                 insert(*new_datum)
             else:
